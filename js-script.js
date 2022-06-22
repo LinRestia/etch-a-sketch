b22407355dab4747c937d7451594833c875e232d
Lin <josephgarcia9919@gmail.com>
2022-06-21 22:06:05 -0700

    finishing some css stylings

--- js-script.js
+++ js-script.js
@@ -1,10 +1,13 @@
 const container = document.querySelector('.container');
 const createGrid = (size) => {
+    if(size > 100 || size < 16) {
+        return 'Please try a number between 16 and 100! Thank You!';
+    };
     for (let i = 0; i < size * size; i++) {
         container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
         container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
         const div = document.createElement('div');
         container.append(div);
-    }
-}
-createGrid(16);
+    };
+};
+createGrid(17);
