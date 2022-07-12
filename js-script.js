f33447ea1b8bbc538b092721971b78a91f72a6a6
Lin <josephgarcia9919@gmail.com>
2022-07-12 16:42:47 -0700

    add a color mode and click and drag to color.

--- js-script.js
+++ js-script.js
@@ -3,19 +3,21 @@ let size = 16;
 const defaultMode = 'draw';
 let currentMode = defaultMode;
 
-const defaultColor = '#ff44ee';
+const defaultColor = '#000000';
 let currentColor = defaultColor;
 
-const defaultType = 'click';
-let currentType = defaultType;
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 const container = document.querySelector('.container');
 const pixelChange = document.querySelector('#pixel-change');
 const eraserBtn = document.querySelector('#eraser');
 const drawBtn = document.querySelector('#draw');
 const rainbowBtn = document.querySelector('#rainbow');
-const clickBtn = document.querySelector('#hover-type');
-const hoverBtn = document.querySelector('#click-type');
+const clearBtn = document.querySelector('#clear');
+const colorBtn = document.querySelector('#color');
+const chooseColor = document.querySelector('#choose-color');
 
 const setCurrentMode = (newMode) => {
     currentMode = newMode;
@@ -23,10 +25,8 @@ const setCurrentMode = (newMode) => {
 const setCurrentColor = (newColor) => {
     currentColor = newColor;
 }
-const setCurrentType = (newType) => {
-    currentType = newType;
-}
 const colorChange = (e) => {
+    if (e.type === 'mouseover' && !mouseDown) return
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     switch(currentMode) {
         case 'draw':
@@ -37,6 +37,9 @@ const colorChange = (e) => {
         break;
         case 'rainbow':
             e.target.style.backgroundColor = `#${randomColor}`;
+        break;
+        case 'color':
+            e.target.style.backgroundColor = currentColor;
         break;
     }
 }
@@ -52,32 +55,48 @@ rainbowBtn.addEventListener('click', () => {
     setCurrentMode('rainbow');
     colorChange;
 });
-hoverBtn.addEventListener('click', () => {
-    setCurrentType('mouseover');
+chooseColor.oninput = (e) => setCurrentColor(e.target.value);
+
+colorBtn.addEventListener('click', () => {
+    setCurrentMode('color');
+    colorChange;
 })
-clickBtn.addEventListener('click', () => {
-    setCurrentType('click');
-})
+clearBtn.addEventListener('click', () => {
+    clearGrid();
+    createGrid(size);
+});
+
 pixelChange.addEventListener('click', () => {
-    size = window.prompt('Please choose a number between 16 and 100!');
+    size = window.prompt('Please choose a number between 16 and 100! Warning: This will clear the canvas.');
+    clearGrid();
     createGrid(size);
 });
 
 const createGrid = (size) => {
     if(size > 100 || size < 16) {
-        return 'Please try a number between 16 and 100! Thank You!';
+        return;
     };
     for (let i = 0; i < size * size; i++) {
         container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
         container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
         container.style.backgroundColor = '#ffffff';
         const div = document.createElement('div');
-        div.classList.add('change')
-        div.addEventListener('click', colorChange);
+        div.classList.add('change');
         div.addEventListener('mouseover', colorChange);
+        div.addEventListener('mousedown', colorChange);
+        div.addEventListener('dragstart', (e) => {
+            e.preventDefault();
+        });
+        div.addEventListener('drop', (e) => {
+            e.preventDefault();
+        });
         container.append(div);
     };
 };
+const clearGrid = () => {
+    container.innerHTML = '';
+};
+
 window.onload = function() {
     createGrid(size);
 };
